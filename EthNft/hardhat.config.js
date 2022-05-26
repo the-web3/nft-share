e7662ece66b79b0cbc1d2c5be12c82173b4afad7
guoshijiang <guoshijiang2012@163.com>
2022-05-26 14:10:03 +0800

    fix

--- EthNft/hardhat.config.js
+++ EthNft/hardhat.config.js
@@ -1,23 +1,43 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("@nomiclabs/hardhat-etherscan");
 
 const ALCHEMY_API_KEY = "KE3V4NLUN3fgvie0lbTHyQ8Q2zSSnfo2";
+const ETHERSCAN_API_KEY = "TNSTBJQHXQV8FJDC8BXEYCQXJP39TJ3U7U"
 const ROPSTEN_PRIVATE_KEY = "a1724a3be3134c9e64d9243428926088c1f4a236e777c19e3c7a974e0da6dba3";
 
+
 module.exports = {
-  solidity: "0.8.1",
+  defaultNetwork: "rinkeby",
+  solidity: {
+    version: '0.8.1',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
+    hardhat: {
+    },
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      url: `https://ropsten.infura.io/v3/1e6e5dce00ac44caba42be9896b8f226`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/1e6e5dce00ac44caba42be9896b8f226`,
       accounts: [`${ROPSTEN_PRIVATE_KEY}`]
     },
     bsctest: {
-      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+      url: `https://data-seed-prebsc-2-s3.binance.org:8545`,
       accounts: [`${ROPSTEN_PRIVATE_KEY}`]
     },
     teletest: {
       url: `https://evm-rpc.testnet.teleport.network`,
       accounts: [`${ROPSTEN_PRIVATE_KEY}`]
     },
+  },
+  etherscan: {
+    apiKey: "TNSTBJQHXQV8FJDC8BXEYCQXJP39TJ3U7U",
   }
 };
--- EthNft/scripts/mint.js
+++ EthNft/scripts/mint.js
@@ -1,7 +1,7 @@
 const Web3 = require("web3")
 
-const contract = require("../artifacts/contracts/SteNft.sol/SteNft.json");
-const API_URL = "https://evm-rpc.testnet.teleport.network"
+const contract = require("../artifacts/contracts/EthNft.sol/EthNft.json");
+const API_URL = "https://rinkeby.infura.io/v3/1e6e5dce00ac44caba42be9896b8f226"
 const ADDRESS = "0xECF09D36f07EC396f97DD448D9E4bcb19fE4Ec3A"
 const PRIVATE_KEY = "a1724a3be3134c9e64d9243428926088c1f4a236e777c19e3c7a974e0da6dba3"
 
@@ -9,7 +9,7 @@ const PRIVATE_KEY = "a1724a3be3134c9e64d9243428926088c1f4a236e777c19e3c7a974e0da
 async function mintNFT(tokenURI) {
     const web3 = new Web3(Web3.givenProvider || API_URL );
     const contract = require("../artifacts/contracts/EthNft.sol/EthNft.json")
-    const contractAddress = "0x75192e880eAad55696b22a1efCA36eff5E889cC9"
+    const contractAddress = "0x0101FA99DA64F117f0Bd7e86b836aD14D0549d02"
     const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
     const nonce = await web3.eth.getTransactionCount(ADDRESS, "latest")
     console.log("nonce is:",  nonce)
